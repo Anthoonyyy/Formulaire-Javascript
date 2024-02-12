089577ca2ac9b402f597a797b56cdaa4d5a4a9ef
Anthoonyyy <Anthony212007@hotmail.com>
2024-02-12 11:54:32 +0100

    js fini

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
-function checkPassword() {
-    var password = document.getElementById('password').value;
+const formContainer = document.querySelector("#formContainer");
 
+formContainer.addEventListener('submit', function(event) {
+    event.preventDefault();});
+
+function checkPassword() {
+    var password = document.getElementById('mdp').value;  
+/*
     var lengthCheck = password.length >= 8;
     var uppercaseCheck = /[A-Z]/.test(password);
     var numberCheck = /\d/.test(password);
-    var symbolCheck = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-    if(lengthCheck){
-        length.style.color = "green";
+    var symbolCheck = /[!@#$%^&*(),.?":{}|<>]/.test(password);      
+ */ 
+var lengthCheck = document.getElementById("lengthCheck");
+var uppercaseCheck = document.getElementById("uppercaseCheck");         // on cherche les span dans HTML pour les changer selon les test suivant :
+var numberCheck = document.getElementById("numberCheck");
+var symbolCheck = document.getElementById("symbolCheck");
+    if(password.length > 7){
+        lengthCheck.style.color = "green";
     }else {
-        length.style.color = "red";
+        lengthCheck.style.color = "red";
     }
-    if(uppercaseCheck) {
+    if(/[A-Z]/.test(password)) {
         uppercaseCheck.style.color = "green";
     }else {
         uppercaseCheck.style.color = "red";
     }
-    if(numberCheck) {
+    if(/\d/.test(password)) {
         numberCheck.style.color = "green";
     }else {
         numberCheck.style.color = "red";
     }
-    if (symbolCheck) {
+    if (/[!@#$%^&*(),.?":{}|<>'-+/]/.test(password)) {        
         symbolCheck.style.color = "green";
     }else {
         symbolCheck.style.color = "red";
     }
+    
 }
-
 function submitForm() {
     var resultDiv = document.getElementById('resultat');
-    var password = document.getElementById('password').value;
+    var password = document.getElementById('mdp').value;    // idem  ligne 7
 
-    if (password.length >= 8 && /[A-Z]/.test(password) && /\d/.test(password) && /[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+    if (password.length >= 8 && /[A-Z]/.test(password) && /\d/.test(password) && /[!@#$%^&*(),.?":{}|<>'-+/]/.test(password)) {
         resultDiv.textContent = "Inscription réussie !";
         resultDiv.style.color = "green";
         setTimeout(function () {
